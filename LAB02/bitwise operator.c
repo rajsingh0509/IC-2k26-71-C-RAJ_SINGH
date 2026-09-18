171e27eabf24c2870f98d2fd8ecd2ad518e63303
Raj singh <rajsingh310526@gmail.com>
2026-09-18 13:17:33 +0530

    Create bitwise operator.c

---
+++ LAB02/bitwise operator.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+
+int a=5;
+int b=3;
+printf("a & b = %d\n", a & b);
+printf("a | b = %d\n", a | b);
+printf("a ^ b = %d\n", a ^ b);
+printf("~a = %d\n", ~a);
+printf("a<<1 = %d\n",a<<1);
+printf("b>>1 = %d\n",b>>1);
+return 0;
+}
