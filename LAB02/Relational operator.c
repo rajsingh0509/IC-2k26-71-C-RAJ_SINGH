4d38d9ca6dcb931f857ee556f89281c842280823
Raj singh <rajsingh310526@gmail.com>
2026-09-18 14:07:29 +0530

    Update Relational operator.c

--- LAB02/Relational operator.c
+++ LAB02/Relational operator.c
@@ -4,7 +4,7 @@ int main() {
 int a=20, b=10;
 
 
-printf("a<b  : %d\n", a<b);        printf("a>b  : %d\n", a>b);
+printf("a<b  : %d\n", a<b);       printf("a>b  : %d\n", a>b);
 printf("a<=b : %d\n", a<=b);
 printf("a>=b : %d\n", a>=b);
 printf("a!=b : %d\n", a!=b);
